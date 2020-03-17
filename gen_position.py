a53cc01b66e4945d15dcb0bfa5fa3093e37b6a89
Soonkyu Jeong <reingel@gmail.com>
2020-03-18 00:52:36 +0900

    coord generator modified

--- gen_position.py
+++ gen_position.py
@@ -1,34 +1,65 @@
 import numpy as np
 
-def find_cubelets(nZero):
+def find_corners():
     ret = []
-    axis = np.array([-1,0,1])
-    for x in axis:
-        for y in axis:
-            for z in axis:
-                coord = np.array([x, y, z])
-                if coord[coord == 0].size == nZero:
-                    s = '(%d, %d, %d)' % (x, y, z)
-                    ret.append(s)
+    tick = np.array([-1,1])
+    for x in tick:
+        for y in tick:
+            for z in tick:
+                s = (x, y, z)
+                ret.append(s)
+    return np.array(ret)
+
+def find_edges():
+    ret = np.array([
+        (0, -1, -1),
+        (0, -1, 1),
+        (0, 1, -1),
+        (0, 1, 1),
+        (-1, 0, -1),
+        (-1, 0, 1),
+        (1, 0, -1),
+        (1, 0, 1),
+        (-1, -1, 0),
+        (-1, 1, 0),
+        (1, -1, 0),
+        (1, 1, 0)
+    ])
     return ret
+
+def write2file(fp):
+    for c in ret:
+        x, y, z = c
+        x1, y1, z1 = c + 1
+        d = 7 * x1 + 2 * y1 + 1 * z1
+        idx.append(d)
+        s = '(%+d %+d %+d) - (%d %d %d) : %d' % (x,y,z, x1,y1,z1, d)
+        print(s)
+        fp.write(s)
+        fp.write('\n')
 
 fp = open('positions.md', 'w')
 
+idx = []
+
 print('# Corner cubelets')
 fp.write('# Corner cubelets\n')
-ret = find_cubelets(0)
-print(ret)
-for c in ret:
-    fp.write('%s\n' % str(c))
+ret = find_corners()
+write2file(fp)
 
 print()
 fp.write('\n')
 
 print('# Edge cubelets')
 fp.write('# Edge cubelets\n')
-ret = find_cubelets(1)
-print(ret)
-for c in ret:
-    fp.write('%s\n' % str(c))
+ret = find_edges()
+write2file(fp)
+
+print()
+fp.write('\n')
+s = str(sorted(idx))
+print(s)
+fp.write(s)
+fp.write('\n')
 
 fp.close()
---
+++ memo.md
@@ -0,0 +1,6 @@
+
+# 설계방향
+- dimension reduction of states
+    - how to represent 20 cubelets
+    - 8 corner cubelets + 12 edge cubelets
+    - group theory
